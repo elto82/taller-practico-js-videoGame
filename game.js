3d2a1ac9c634ae4ea4e53bc9a7e3db9aa98ad7e8
Argiro <elto.82@gmai.com>
2022-10-14 07:03:06 -0500

    arreglos multidimensionales

--- game.js
+++ game.js
@@ -26,10 +26,18 @@ function setCanvasSize() {
 function startGame() {
   console.log({ canvasSize, elementsSize });
 
-  game.font = elementsSize + "px Verdana";
+  game.font = elementsSize + "px Roboto";
   game.textAlign = "end";
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis["X"], elementsSize, elementsSize * i);
+  const map = maps[1];
+  const mapRows = map.trim().split('\n');
+  const mapRowCols = mapRows.map(row => row.trim().split(''));
+  console.log({map,mapRows, mapRowCols});
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      game.fillText(emojis[mapRowCols[row - 1][col - 1]],
+        elementsSize * col, elementsSize * row);      
+    }
   }
 }
