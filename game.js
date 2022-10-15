5d04d95f0f6768c0708fe49441d867de60ad811a
Argiro <elto.82@gmai.com>
2022-10-15 15:57:39 -0500

    Objeto playerPosition

--- game.js
+++ game.js
@@ -8,6 +8,11 @@ const btonDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 
@@ -49,9 +54,23 @@ function startGame() {
       const emoji = emojis[col];
       const posX = elementsSize * (colI + 1);
       const posY = elementsSize * (rowI + 1);
+
+      if(col == 'O'){
+        //console.log(posX, posY);
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+        console.log({playerPosition});
+      }
+
       game.fillText(emoji, posX, posY);
     });
   });
+
+  movePlayer();
+}
+
+function movePlayer (){
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
 }
 
 window.addEventListener('keydown', moveByKeys);
@@ -70,6 +89,8 @@ function moveByKeys(event){
 
 function moveUp(){
   console.log('arriba');
+  playerPosition.y -= elementsSize
+  movePlayer();
 }
 function moveLeft(){
   console.log('izquierda');
